aa80e5ee897b0307022014aab428b93b0f5647ac
Mateus Neves <mateusgameroficialbr@gmail.com>
2026-09-02 15:15:19 -0300

    Criado o basico dos codigos, falta o Controller do Norget para testar os botões, Supostamente a mecanica das notas está funcional

---
+++ Assets/Scripts/Notes/ChartData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// acho que isso seria o chart?
+
+public enum NoteType { AttackUp, AttackDown } //  Quantidade de notas, no caso 2?
+
+[System.Serializable]
+public class NoteEvent {
+    public float beat;      // em que beat a nota acontece
+    public NoteType type;
+}
+
+[CreateAssetMenu(fileName = "NewChart", menuName = "RitmoGalinha/Chart")]
+public class ChartData : ScriptableObject {
+    public AudioClip song;
+    public float bpm;
+    public List<NoteEvent> notes = new List<NoteEvent>();
+}
---
+++ Assets/Scripts/Notes/Note.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Note : MonoBehaviour {
+    public NoteType Type {get; private set;}
+    public float TargetTime {get; private set;} // Momento exato em segundos da musica que deveria ser acertada
+
+    private Vector3 startPos, EndPos;
+    private float travelTime;
+    private float spawnTime;
+
+    public void Init(NoteType type, float targetTime, Vector3 start, Vector3 end, float travel)
+    {
+        Type = type;
+        TargetTime = targetTime;
+        startPos = start;
+        EndPos = end;
+        travelTime = travel;
+        spawnTime = (float)Conductor.Instance.SongPositionInSeconds;
+    }
+
+    void Update()
+    {
+        float elapsed = (float)Conductor.Instance.SongPositionInBeats - spawnTime;
+        float t = elapsed / travelTime;
+        transform.position = Vector3.Lerp(startPos, EndPos, t);
+        
+        //se passou da hitzone é não foi acertada, dá miss
+        if (t > 1.15f)
+        {
+            JudgementSystem.Instance.RegisterMiss();
+            Destroy(gameObject);
+        }
+            
+    }
+}
